45a9a8aca14aae927f9f5406f5e1501dd8351ad3
Bruno Borges <brborges@microsoft.com>
2023-12-04 00:32:25 -0800

    Use Math.pow instead of a while loop

--- src/AoC_04.java
+++ src/AoC_04.java
@@ -35,22 +35,22 @@ public class AoC_04 {
         public Card(String input) {
             var parts = input.split(":");
             this.cardNumber = Integer.parseInt(parts[0].trim().split("\\s+")[1]);
+
             var numbersGroups = parts[1].trim().split("\\|");
+            winningNumbers = Arrays.stream(numbersGroups[0].trim().split("\\s+"))
+                    .map(Integer::parseInt)
+                    .toList();
 
-            var _winningNumbers = numbersGroups[0].trim().split("\\s+");
-            winningNumbers = Arrays.asList(_winningNumbers).stream().map(Integer::parseInt).toList();
+            myNumbers = Arrays.stream(numbersGroups[1].trim().split("\\s+"))
+                    .map(Integer::parseInt)
+                    .toList();
 
-            var _myNumbers = numbersGroups[1].trim().split("\\s+");
-            myNumbers = Arrays.asList(_myNumbers).stream().map(Integer::parseInt).toList();
+            myWinningNumbers = myNumbers.stream()
+                    .filter(winningNumbers::contains)
+                    .toList();
 
-            myWinningNumbers = myNumbers.stream().filter(winningNumbers::contains).toList();
-
-            if (myWinningNumbers.size() > 0) {
-                cardPoints = 1;
-                int remainingNumbers = myWinningNumbers.size() - 1;
-                while (remainingNumbers-- > 0) {
-                    cardPoints *= 2;
-                }
+            if (!myWinningNumbers.isEmpty()) {
+                cardPoints = (int) Math.pow(2, myWinningNumbers.size() - 1);
             }
         }
     }
